83d029c3f63229be38056b57b0e985c6da2d6193
Manuel Maisano <manumaisano@gmail.com>
2024-03-25 16:06:01 +0100

    FIST COMMIT SHEET/ ARRAY TABLE

---
+++ script.js
@@ -0,0 +1,16 @@
+//SNACK 1
+
+const Table = [
+ 'Brad Pitt',
+ 'Johnny Depp', 
+ 'Lady Gaga', 
+ 'Cristiano Ronaldo', 
+ 'Georgina Rodriguez', 
+ 'Chiara Ferragni',  
+ 'George Clooney', 
+ 'Amal Clooney', 
+ 'Fedez', 
+ 'Amadeus', 
+ 'Fiorello']
+ 
+console.log(Table);
