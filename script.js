0f03c04fb83103daf2779ed609ba54baadfaa301
Manuel Maisano <manumaisano@gmail.com>
2024-03-26 14:09:30 +0100

    racingbike snack3

--- script.js
+++ script.js
@@ -94,3 +94,35 @@ console.log(Grade);
 
 const idandgrade = students.filter((element) => element.Grade >= 70 && element.id >= 120);
 console.log(idandgrade);
+
+
+//SNACK 3
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+
+
+const racingbike =  [
+    {
+        modelName : 'CERVÉLO R5',
+        weight : 7000,
+    },
+    {
+        modelName : 'COLNAGO V4RS',
+        weight : 6990,
+    },
+    {
+        modelName : 'SPECIALIZED S-WORKS SL8 ED AETHOS',
+        weight : 6620,
+    },
+    {
+        modelName : 'FACTOR O2 VAM 2023',
+        weight : 6800,
+    },
+    {
+        modelName : 'ORBEA ORCA 2024',
+        weight : 6700,
+    },
+
+];
